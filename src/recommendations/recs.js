54ec49b4970cd9780732f83c9b8723dce21af516
nir <nirr0313@gmail.com>
2025-02-21 11:04:39 +0530

    created recss system

---
+++ src/recommendations/recs.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./recs.css"
+
+export default function Recs() {
+  return (
+    <div>
+      <h2>Product Recommendations</h2>
+      {/* ...the rest of your code... */}
+    </div>
+  );
+}
